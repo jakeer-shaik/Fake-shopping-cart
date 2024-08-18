d3a985025ec73a84dee635a3fb15ea461e49e9c8
jakeer-shaik <jakeershaik202@gmail.com>
2024-08-18 11:31:11 +0530

    configuring

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'fakestoreapi.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'media.istockphoto.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'via.placeholder.com',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
